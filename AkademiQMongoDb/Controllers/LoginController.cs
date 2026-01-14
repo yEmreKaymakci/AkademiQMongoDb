9fa63bff23a684e7a135bcb33ab86c37d0870d27
yEmreKaymakci <yunusemrekymkci@gmail.com>
2026-01-14 22:17:41 +0300

    commit

---
+++ AkademiQMongoDb/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using AkademiQMongoDb.DTOs.AdminDtos;
+using AkademiQMongoDb.Services.AdminServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.Controllers
+{
+    public class LoginController(IAdminService _adminService) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginAdminDto loginAdminDto)
+        {
+            var result = await _adminService.LoginAdminAsync(loginAdminDto);
+            if (result is false)
+            {
+                ModelState.AddModelError("", "Kullanıcı Adı veya Şifre Hatalı, ya da henüz kaydınız onaylanmamış.");
+                return View(loginAdminDto);
+            }
+            return Redirect("/Admin/Product/Index");
+        }
+    }
+}
---
+++ AkademiQMongoDb/ViewComponents/DefaultComponents/_DefaultProductsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using AkademiQMongoDb.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultProductsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public _DefaultProductsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+        //Invoke methodu eğer asenkronk olursa  InvokeAsync olarak yazılmalı!!
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var product = await _productService.GetAllAsync();
+            return View(product);
+        }
+
+    }
+}
