50004103a28bbd59be2780d711999d059ab8f26a
yEmreKaymakci <yunusemrekymkci@gmail.com>
2026-02-07 03:13:19 +0300

    commit

---
+++ AkademiQMongoDb/Services/ChefServices/ChefService.cs
@@ -0,0 +1,45 @@
+﻿using AkademiQMongoDb.DTOs.ChefDtos;
+using AkademiQMongoDb.Entities;
+using AkademiQMongoDb.Settings;
+using MongoDB.Driver;
+
+namespace AkademiQMongoDb.Services.ChefServices
+{
+    public class ChefService : IChefService
+    {
+        private readonly IMongoCollection<Chef> _chefCollection;
+
+        public ChefService(IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _chefCollection = database.GetCollection<Chef>(databaseSettings.ChefCollectionName);
+
+        }
+
+        public Task CreateAsync(CreateChefDto chefDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultChefDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateChefDto> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(UpdateChefDto chefDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AkademiQMongoDb/Services/ChefServices/IChefService.cs
+++ AkademiQMongoDb/Services/ChefServices/IChefService.cs
@@ -1,6 +1,13 @@
-﻿namespace AkademiQMongoDb.Services.ChefServices
+﻿using AkademiQMongoDb.DTOs.ChefDtos;
+
+namespace AkademiQMongoDb.Services.ChefServices
 {
     public interface IChefService
     {
+        Task<List<ResultChefDto>> GetAllAsync();
+        Task<UpdateChefDto> GetByIdAsync(string id);
+        Task CreateAsync(CreateChefDto chefDto);
+        Task UpdateAsync(UpdateChefDto chefDto);
+        Task DeleteAsync(string id);
     }
 }
