5e8376eadd83a7ab36ce703a851f4df1770e14cc
yEmreKaymakci <yunusemrekymkci@gmail.com>
2026-02-17 14:36:23 +0300

    commit

---
+++ AkademiQMongoDb/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.Controllers
+{
+    [AllowAnonymous]
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
